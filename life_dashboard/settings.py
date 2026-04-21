aaf1c2736ad6c923f9d73a3e41bd41c5e46dcc1b
BHARATHKUMAR <bk3007155@gmail.com>
2026-04-21 12:37:54 +0530

    create MIDDLEWARE

--- life_dashboard/settings.py
+++ life_dashboard/settings.py
@@ -16,11 +16,21 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',  # REQUIRED
             ],
         },
     },
 ]
-
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',   # REQUIRED
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',  # REQUIRED
+    'django.contrib.messages.middleware.MessageMiddleware',     # REQUIRED
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 STATIC_URL = 'static/'
